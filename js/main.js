d3f6c951cd7ae16fe5711dc88c9c6d2e6b1c8be8
ClemenPine <ClemenPine@pm.me>
2023-03-31 18:10:32 -0400

    added an about page

--- js/main.js
+++ js/main.js
@@ -18,28 +18,3 @@ window.onload = function() {
 
     game.init()
 }
-
-window.theme = function() {
-    const toggle = document.getElementById('theme-toggle')
-    const icon = toggle.querySelector('i')
-
-    const html = document.querySelector('html')
-
-    if (html.classList.contains('dark')) {
-        html.classList.remove('dark')
-
-        icon.classList.remove('fa-moon')
-        icon.classList.add('fa-sun')
-        toggle.setAttribute('title', 'Dark Mode')
-
-        document.body.style.backgroundImage = "url('assets/light.jpg')"
-    } else {
-        html.classList.add('dark')
-
-        icon.classList.remove('fa-sun')
-        icon.classList.add('fa-moon')
-        toggle.setAttribute('title', 'Light Mode')
-
-        document.body.style.backgroundImage = "url('assets/dark.jpg')"
-    }
-}
